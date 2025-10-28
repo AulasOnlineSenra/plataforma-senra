1d114d65ef13d73afb23ccc21059936fe23cf148
Aulas Online Senra <senraaulasonline@gmail.com>
2025-10-28 17:45:22 +0000

    can you decrease width 50px (_for element div_)

--- src/components/dashboard-client-layout.tsx
+++ src/components/dashboard-client-layout.tsx
@@ -33,7 +33,7 @@ function DashboardLayoutContent({ children }: { children: React.ReactNode }) {
         maxSize={20}
         className={cn(
           'hidden md:block transition-all duration-300 ease-in-out min-w-[50px]',
-          !isCollapsed && 'md:min-w-[220px] lg:min-w-[280px]'
+          !isCollapsed && 'md:min-w-[170px] lg:min-w-[230px]'
         )}
       >
         <div className="h-full border-r bg-sidebar">
