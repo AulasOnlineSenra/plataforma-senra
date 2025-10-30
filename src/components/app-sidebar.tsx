b8d4b8b431e2bd096e22245778b0cd92b46de036
Aulas Online Senra <senraaulasonline@gmail.com>
2025-10-30 16:13:03 +0000

    também deve ajustar a posição dos botões do rodapé da sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -296,7 +296,7 @@ export function AppSidebar({ isMobile = false, isCollapsed = false }: { isMobile
 
   return (
     <aside className={cn(
-        "flex h-full max-h-screen flex-col gap-2 w-full group", 
+        "flex h-full max-h-screen flex-col w-full group", 
         isMobile ? '' : 'hidden sm:flex bg-sidebar text-sidebar-foreground',
         isCollapsed && 'is-collapsed'
     )}>
@@ -314,7 +314,7 @@ export function AppSidebar({ isMobile = false, isCollapsed = false }: { isMobile
               </div>
           </div>
         </Link>
-        <div className="flex-1 overflow-y-auto">
+        <div className="flex-1 overflow-y-auto overflow-x-hidden">
                 <nav className={cn("grid items-start gap-1 px-2 text-sm font-medium lg:px-4", isMobile ? 'py-4' : '')}>
                     {filteredNavItems.map(item => renderLink(item, false, 'main'))}
                     {filteredAdminNavItems.length > 0 && (
