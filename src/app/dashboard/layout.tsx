2e7ae58c45dcbbc4f09519ebab69e3b73146fa6d
Aulas Online Senra <senraaulasonline@gmail.com>
2025-11-04 15:42:38 +0000

    da pra diminuir mais?

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
 }) {
   return (
     <DashboardClientLayout>
-      <div className="w-full max-w-7xl mx-auto">
+      <div className="w-full max-w-6xl mx-auto">
         {children}
       </div>
     </DashboardClientLayout>
