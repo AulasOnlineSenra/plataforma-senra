2f0bb3f7521d996e617acc4265cd87bf0a8ea3ec
Aulas Online Senra <senraaulasonline@gmail.com>
2025-11-04 15:39:30 +0000

    pode diminuir a largura total desse container e todos os containers que

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
 }) {
   return (
     <DashboardClientLayout>
-      <div className="w-full max-w-screen-2xl mx-auto">
+      <div className="w-full max-w-7xl mx-auto">
         {children}
       </div>
     </DashboardClientLayout>
